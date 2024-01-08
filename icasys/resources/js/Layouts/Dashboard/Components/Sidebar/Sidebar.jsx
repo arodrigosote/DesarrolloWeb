a0f2dbb493e9b8f48e1f1f4a5bba8e5649ecbb33
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-01-08 00:15:27 -0600

    Update Sidebar.jsx

--- icasys/resources/js/Layouts/Dashboard/Components/Sidebar/Sidebar.jsx
+++ icasys/resources/js/Layouts/Dashboard/Components/Sidebar/Sidebar.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import './Sidebar.css';
 import NavLink from '@/Components/NavLink';
 import '../../../../../css/app.css';
-import { RiDashboardFill, RiMenuFill, RiCloseFill, RiCompass2Fill  } from "react-icons/ri";
+import { RiDashboardFill, RiMenuFill, RiCloseFill, RiCompass2Fill } from "react-icons/ri";
 import icono from '../../../../Assets/Images/blanco.webp';
+import { Link } from "@inertiajs/react";
 
 export default function () {
 
@@ -16,25 +17,26 @@ export default function () {
     return (
         <>
             {/* Sidebar */}
-            <div className={`fixed lg:static top-0 transition-all ${sidebar ? "-left-0" : "-left-full"} lg:w-full md:w-[40%] w-[80%] h-full  col-span-1 p-7 bg-primary`}>
+            <div className={`fixed lg:static top-0 transition-all ${sidebar ? "-left-0" : "-left-full"} lg:w-full md:w-[40%] w-[80%] h-full  col-span-1 bg-primary`}>
 
                 {/* Logotipo */}
-                <div className="text-center pt-3 pb-3 lg:flex lg:justify-start lg:items-center">
-                    <img src={icono} alt="" className="w-1/4 mx-auto md:mx-auto lg:mx-0" />
-                    <h1 className="uppercase font-bold tracking-[4px] text-white">ICASYS</h1>
-                </div>
+                <Link href={route('page.home')}>
+                    <div className="text-center py-3 lg:pl-4 lg:flex lg:justify-start lg:items-center border-b-2">
+                        <img src={icono} alt="" className="w-1/6 mx-auto md:mx-auto lg:mx-0" />
+                        <h1 className="uppercase text-gray-200 tracking-[1px] hover:text-white">ICASYS</h1>
+                    </div>
+
+                </Link>
 
                 {/* Menu */}
-                <nav>
-                    <ul>
+                <nav className="p-2">
+                    <ul className="">
                         <li className="flex pb-2">
-                            <NavLink to={route('dashboard.home')} className="hover:bg-white h-text-blue text-white w-full pt-3 pb-3 pl-4 gap-2 rounded-xl font-bold transition-colors text-lg"><RiDashboardFill className="text-xl"></RiDashboardFill>Escritorio</NavLink>
+                            <Link href={route('dashboard.home')} className="hover:bg-white h-text-blue text-white w-full pt-3 pb-3 pl-4 gap-2 rounded-xl font-bold transition-colors text-lg flex items-center justify-start text-sm"><RiDashboardFill className="text-xl"></RiDashboardFill >Escritorio</Link>
                         </li>
+
                         <li className="flex pb-2">
-                            <NavLink to='' className="hover:bg-white h-text-blue text-white w-full pt-3 pb-3 pl-4 gap-2 rounded-xl font-bold transition-colors text-lg"><RiDashboardFill className="text-xl"></RiDashboardFill>Escritorio</NavLink>
-                        </li>
-                        <li className="flex pb-2">
-                            <NavLink to={route('horarios.index')} className="hover:bg-white h-text-blue text-white w-full pt-3 pb-3 pl-4 gap-2 rounded-xl font-bold transition-colors text-lg"><RiCompass2Fill  className="text-xl"></RiCompass2Fill >Horarios</NavLink>
+                            <Link href={route('horarios.index')} className="hover:bg-white h-text-blue text-white w-full pt-3 pb-3 pl-4 gap-2 rounded-xl font-bold transition-colors text-lg flex items-center justify-start text-sm"><RiCompass2Fill className="text-xl"></RiCompass2Fill >Horarios</Link>
                         </li>
                     </ul>
                 </nav>
