3fb1c8b1b8e423fe0d30e7a7bc1793161ad8f75c
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-06-27 20:43:09 -0600

    update reserva-tienda

--- reservas-tienda/resources/js/Components/FormBook/BookForm.jsx
+++ reservas-tienda/resources/js/Components/FormBook/BookForm.jsx
@@ -1,13 +1,21 @@
-
+import '../../../css/app.css'
 
 export default function BookForm () {
     return(
         <section className="my-12">
             <div className="bg-color5 block lg:flex justify-center align-middle">
-                <div className="bg-color5">
-                    aqui paosos
+                <div className="bg-color4 block">
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
+                    <li className='list-none'>Ubicación</li>
                 </div>
                 <div>
+
                     aqui form
                 </div>
             </div>
